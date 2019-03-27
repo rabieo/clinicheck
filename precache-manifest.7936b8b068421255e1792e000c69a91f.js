8361d68984664f01e48217bd2fa1a2d1408371e5
rabieo <43918617+rabieo@users.noreply.github.com>
2019-03-27 04:03:59 -0400

    Updates

---
+++ precache-manifest.7936b8b068421255e1792e000c69a91f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c0498d51e52114e25f041107bc102f53",
+    "url": "/clinicheck/static/media/queue1.c0498d51.png"
+  },
+  {
+    "revision": "4d8dba3e60c3085879ba",
+    "url": "/clinicheck/static/js/runtime~main.dc42ea4d.js"
+  },
+  {
+    "revision": "000c6e4672dfdf8d8bcf",
+    "url": "/clinicheck/static/js/main.6ee0c0ea.chunk.js"
+  },
+  {
+    "revision": "15c57eea925593ebf3a0",
+    "url": "/clinicheck/static/js/2.81d34ce9.chunk.js"
+  },
+  {
+    "revision": "000c6e4672dfdf8d8bcf",
+    "url": "/clinicheck/static/css/main.d03156dd.chunk.css"
+  },
+  {
+    "revision": "d2c7905f52e93c4db1bf9c34ee5e883c",
+    "url": "/clinicheck/index.html"
+  }
+];
